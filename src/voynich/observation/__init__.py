32f61936d8c1926a0b8b7c0f19820d158d9802cc
Breno Andrade <brenowilsondeandradesilva@gmail.com>
2026-07-06 19:17:40 -0300

    feat: export observation lifecycle utilities

--- src/voynich/observation/__init__.py
+++ src/voynich/observation/__init__.py
@@ -1,5 +1,11 @@
 """Interpretation-neutral source-anchored observation utilities."""
 
+from .lifecycle import (
+    ObservationLifecycleError,
+    freeze_draft,
+    start_draft,
+    validate_freeze_record,
+)
 from .model import (
     ObservationValidationError,
     build_blank_package,
@@ -8,8 +14,12 @@ from .model import (
 )
 
 __all__ = [
+    "ObservationLifecycleError",
     "ObservationValidationError",
     "build_blank_package",
     "canonical_sha256",
+    "freeze_draft",
+    "start_draft",
+    "validate_freeze_record",
     "validate_package",
 ]
